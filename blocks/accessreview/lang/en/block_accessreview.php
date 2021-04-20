02780c6866d80e64d8e3c260335445f369b8a218
Andrew Nicols <andrew@nicols.co.uk>
2021-04-20 08:02:28 +0800

    MDL-68963 block_accessreview: Generate inline review with templates
    We muts not generate HTML content manually via string concatenation.
    This patch modifies the block to use a mustache template instead,
    allowing themers to make any changes they desire.
    
    Images are copied into the block, instead of using the same variants in
    core as per coding style. This allows themers to use different icons if
    they prefer.
    
    The patch also removes the need for module-specific hacks (i.e. label),
    by using a CSS class with higher specificity to capture those places
    affected.
    
    Finally language strings are composed via get_string rather than string
    concatenation.

--- blocks/accessreview/lang/en/block_accessreview.php
+++ blocks/accessreview/lang/en/block_accessreview.php
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+
 /**
  * Simple block language strings
  *
@@ -35,8 +36,6 @@ $string['guides'] = 'Guides';
 $string['link:pdf'] = 'Link to PDF resource on {$a} - opens in new window';
 $string['link:video'] = 'Link to Video resource on {$a} - opens in new window';
 $string['nodata'] = 'No accessibility results data was found.';
-$string['passrate'] = 'Pass rate';
-$string['percenterrors'] = '% of all checks';
 $string['pluginname'] = 'Accessibility Review';
 $string['privacy:metadata'] = 'The Accessibility Review plugin does not store any personal data.';
 $string['showbackground'] = 'Show background colouring only';
@@ -46,6 +45,10 @@ $string['showicons'] = 'Show count icons only';
 $string['showint'] = 'Display amount';
 $string['showpercent'] = 'Display percentage';
 $string['smile'] = '0 errors, congratulations!';
+$string['status:successalt'] = 'Passed';
+$string['status:success'] = 'Passed';
+$string['status:errors'] = 'Failed {$a->errorCount} errors ({$a->failureRate}% of all checks)';
+$string['status:errorsalt'] = 'Errors';
 $string['toggleaccessreview'] = 'Toggle highlighting';
 $string['toolpage'] = 'Report page to show';
 $string['totalerrors'] = '<em>Total errors:</em> {$a} <br>(total excludes course settings)';

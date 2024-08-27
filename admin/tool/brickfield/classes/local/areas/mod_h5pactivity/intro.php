9d38157eead77348f1515d648ff179a3e751d645
Max Larkin <maxlarkin@protonmail.com>
2024-08-27 18:38:11 +0100

    MDL-82870 block_accessreview: Add clear heatmap messaging

---
+++ admin/tool/brickfield/classes/local/areas/mod_h5pactivity/intro.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace tool_brickfield\local\areas\mod_h5pactivity;
+
+use tool_brickfield\local\areas\module_area_base;
+
+/**
+ * H5P intro observer.
+ *
+ * @package    tool_brickfield
+ * @copyright  2024 onward: Brickfield Education Labs, www.brickfield.ie
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class intro extends module_area_base {
+
+    /**
+     * Return the area table name.
+     * @return string
+     */
+    public function get_tablename(): string {
+        return 'h5pactivity';
+    }
+
+    /**
+     * Return the area field name.
+     * @return string
+     */
+    public function get_fieldname(): string {
+        return 'intro';
+    }
+}
